aec0a97899cd8af6ec7b5c29b8e29624df7f6bc8
Ridun <ridundasvv@gmail.com>
2020-07-16 12:02:19 +0530

    commited to check eclipse git operation

--- src/test/java/CIAutomation/MavenJenkinsGit/Thirdfilecreatedeclipse.java
+++ src/test/java/CIAutomation/MavenJenkinsGit/Thirdfilecreatedeclipse.java
@@ -5,6 +5,7 @@ public class Thirdfilecreatedeclipse {
 	public static void main(String[]args)
 	{
 		System.out.println("3rd file added via eclipse");
+		System.out.println("Added to check eclipse commit");
 	}
 
 }
